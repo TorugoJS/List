37d74de4c2b34cdca0e9eaaa412e52e2616352c6
TorugoJS <victor.devfrontend@gmail.com>
2022-07-26 19:42:02 -0300

    list-todo

---
+++ components/header.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import { BsTrash, BsbookmarkCheck, BsbookmarkCheckFill } from "react-icons/bs";
+
+
+
+function Header(){
+    return(
+
+<h1>a</h1>
+    )
+}
+
+export default Header;
